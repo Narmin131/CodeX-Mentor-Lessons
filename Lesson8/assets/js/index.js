74233933cbfe8b24e11d69dfa414274ee61c198a
Narmin <mnermin064@gmail.com>
2023-01-25 12:11:04 +0400

    Example site

---
+++ Lesson8/assets/js/index.js
@@ -0,0 +1,23 @@
+// ES 5
+
+function myFunction() {
+    console.log('Salam');
+}
+
+myFunction()
+
+
+function myFunc(a, b) {
+    console.log(a * b);
+}
+
+myFunc(10, 20)
+
+
+// ES 6
+
+const func2 = () => {
+    console.log('ES6 function');
+}
+
+func2()
